e0cf9105e23315894683f81ca0cf3b3ae9ef9995
filipemartinsdev <dev.filipemartins@gmail.com>
2026-07-23 18:34:41 -0300

    feat: finish consumer configuration

--- consumer/src/main/java/sandbox/consumer/infra/events/EventConsumer.java
+++ consumer/src/main/java/sandbox/consumer/infra/events/EventConsumer.java
@@ -5,21 +5,25 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import sandbox.consumer.application.dto.event.NewTemperatureEvent;
 import sandbox.consumer.application.service.TelemetryService;
+import tools.jackson.databind.ObjectMapper;
 
 @Slf4j
 @Component
 public class EventConsumer {
     private final TelemetryService telemetryService;
+    private final ObjectMapper objectMapper;
 
-    public EventConsumer(TelemetryService telemetryService) {
+    public EventConsumer(TelemetryService telemetryService, ObjectMapper objectMapper) {
         this.telemetryService = telemetryService;
+        this.objectMapper = objectMapper;
     }
 
     @KafkaListener(
             topics = "sensor.temperature.updated",
             groupId = "telemetry-service"
     )
-    public void consume(NewTemperatureEvent event){
+    public void consume(String json){
+        NewTemperatureEvent event = objectMapper.readValue(json, NewTemperatureEvent.class);
         log.info("Received temperature {}º", event.value());
         telemetryService.handleNewTemperature(event);
     }
