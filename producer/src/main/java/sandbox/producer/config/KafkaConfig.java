4d7604aa2847c06b7511ea90131b995f44dc5b47
filipemartinsdev <dev.filipemartins@gmail.com>
2026-07-27 11:29:45 -0300

    fix(producer): finish kafka stream config and remove unnecessary logs

--- producer/src/main/java/sandbox/producer/config/KafkaConfig.java
+++ producer/src/main/java/sandbox/producer/config/KafkaConfig.java
@@ -22,14 +22,14 @@ import java.util.UUID;
 @Configuration @Profile("!test")
 @EnableKafkaStreams
 public class KafkaConfig {
-    @Value("${app.alerts.temperature-limit:40}")
+    @Value("${app.alerts.temperature-limit:20}")
     private float TEMP_LIMIT;
 
     @Bean
     public NewTopic temperatureTopic() {
         return TopicBuilder.name("sensor.temperature.updated")
                 .replicas(1)
-                .partitions(1)
+                .partitions(3)
                 .build();
     }
 
@@ -56,8 +56,8 @@ public class KafkaConfig {
                 Consumed.with(Serdes.String(), eventSerde)
         );
 
-//        Step 3: Sliding Window of 5 minutes
-        TimeWindows timeWindows = TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(5));
+//        Step 3: Sliding Window of 1 minute
+        TimeWindows timeWindows = TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(1));
 
 //        Step 4: Pipeline definition
         sourceStream
@@ -71,7 +71,7 @@ public class KafkaConfig {
 
 //                Stateless -> Stateful
                 .aggregate(
-                        () -> new TemperatureAccumulator(0, 0L), // Initial value
+                        () -> new TemperatureAccumulator(0.0f, 0L), // Initial value
                         (key, event, accum) -> {
                             return new TemperatureAccumulator(accum.sum() + event.value(), accum.count() + 1);
                         },
@@ -86,8 +86,8 @@ public class KafkaConfig {
                 .map((windowedKey, accum) -> {
                     String componentId = windowedKey.key();
                     String alertMessage = String.format(
-                            "[ALERT] The sensor %s exceeded the temperature limit. Last 5 minutes average: %.2fº",
-                            componentId, accum.getAverage()
+                            "The sensor %s exceeded the temperature limit of %.2f°. Last 1 minute average: %.2f°",
+                            componentId, TEMP_LIMIT, accum.getAverage()
                     );
                     return new KeyValue<>(componentId, new TemperatureAlertEvent(UUID.fromString(componentId), alertMessage, Instant.now()));
                 })
@@ -97,11 +97,9 @@ public class KafkaConfig {
         return sourceStream;
     }
 
-
     private static record TemperatureAccumulator (float sum, long count){
         public float getAverage(){
             return count == 0 ? 0.0f : sum / count;
         }
     };
 }
-
